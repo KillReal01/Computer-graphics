35ef06beb3d0eea37bace0dd783082fcb09fdce7
BagaevDanil <MrBagaevDanil@yandex.ru>
2023-09-17 19:47:28 +0300

    add Draw feature to Canvas

--- lab1/Canvas/Canvas.cpp
+++ lab1/Canvas/Canvas.cpp
@@ -1,9 +1,10 @@
 #include "Canvas.h"
 #include "ui_Canvas.h"
 
-Canvas::Canvas(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::Canvas)
+Canvas::Canvas(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Canvas)
+    , _pen(QPen(Qt::black, 1, Qt::SolidLine, Qt::FlatCap))
 {
     ui->setupUi(this);
 }
@@ -11,4 +12,19 @@ Canvas::Canvas(QWidget *parent) :
 Canvas::~Canvas()
 {
     delete ui;
+}
+
+void Canvas::DrawFigure(const Figure& figure)
+{
+    _cur_figure = figure;
+    repaint();
+}
+
+void Canvas::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+    QPainter painter(this);
+    painter.setPen(_pen);
+
+    painter.drawEllipse(100, 50, 150, 150);
 }
